1d830e4d18901a0029f392cad3165cff148fbf5d
lunarlunarll123 <lunarlightgg@gmail.com>
2025-02-12 17:38:49 -0500

    Delete 3_layer_architecture_course-master directory

--- 3_layer_architecture_course-master/business/exceptions.py
+++
@@ -1,5 +0,0 @@
-class UserNotFoundException(Exception):
-    pass
-
-class InvalidUserException(Exception):
-    pass
--- 3_layer_architecture_course-master/main.py
+++
@@ -1,8 +0,0 @@
-from presentation.app_controller import create_app
-
-def main():
-    app = create_app()
-    app.run(debug=True)
-
-if __name__ == "__main__":
-    main()
